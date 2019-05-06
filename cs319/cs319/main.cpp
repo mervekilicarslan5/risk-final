4419376086ca3f8566066ced48cc6a36c2548277
burakintisah <burak.intisah@gmail.com>
2019-05-07 02:08:08 +0300

    NetworkTrial2

--- cs319/cs319/main.cpp
+++ cs319/cs319/main.cpp
@@ -3,6 +3,9 @@
 
 #include <SFML/Graphics.hpp>
 #include <SFML/Network.hpp>
+#include <SFML/Audio.hpp>
+#include <map>
+#include <conio.h>
 
 #include <iostream>
 #include <string>
@@ -22,83 +25,82 @@ int main() {
 	//GM->startGame();
 
 	IpAddress ip = IpAddress::getLocalAddress();
-	TcpSocket socket;
-	char connectionType;
+	UdpSocket socket;
+	char connectionType, mode;
+
+	map<unsigned short, IpAddress> computerID;
+	string text = " asd";
+	Packet packet;
+	
+	cout << ip << endl;
 
 	cout << "(s) for server, (c) for client: ";
 	cin >> connectionType;
 
+	unsigned short port;
+
+	cout << "Set port number: ";
+	cin >> port;
+
+	if (socket.bind(port) != Socket::Done) {
+		cout << "Couldnt binded. ";
+	}
+	else {
+		cout << "BINDED !! " << endl;
+	}
+
+	//socket.setBlocking(false);
+
 	if (connectionType == 's') {
-		TcpListener listener;
-		listener.listen(2000);
-		listener.accept(socket);
-	}
-	else
-		socket.connect(ip, 2000);
-
-	RectangleShape rect1, rect2;
-
-	rect1.setSize(Vector2f(20, 20));
-	rect2.setSize(Vector2f(20, 20));
-	
-	rect1.setFillColor(Color::Red);
-	rect2.setFillColor(Color::Blue);
-
-	RenderWindow Window(VideoMode(800, 600, 32), "Packets");
-
-	Vector2f prevPosition, p2Position;
-
-	socket.setBlocking(false);
-
-	bool update = false;
-
-	while (Window.isOpen()) {
-
-		Event Event;
-		while (Window.pollEvent(Event)) {
-			if (Event.type == Event::Closed || Event.key.code == Keyboard::Escape)
-				Window.close();
-			else if (Event.type == Event::GainedFocus)
-				update = true;
-			else if (Event.type == Event::LostFocus)
-				update = false;
-
-		}
-	
-		prevPosition = rect1.getPosition();
-
-		if (update) {
-			if (Keyboard::isKeyPressed(Keyboard::Right))
-				rect1.move(1.0f, 0.0f);
-			else if (Keyboard::isKeyPressed(Keyboard::Left))
-				rect1.move(-1.0f, 0.0f);
-			else if (Keyboard::isKeyPressed(Keyboard::Up))
-				rect1.move(0.0f, -1.0f);
-			else if (Keyboard::isKeyPressed(Keyboard::Down))
-				rect1.move(0.0f, 1.0f);
-		}
-
-		Packet packet;
-
-		if (prevPosition != rect1.getPosition()) {
-			packet << rect1.getPosition().x << rect1.getPosition().y;
-			socket.send(packet);
-		}
-
-		socket.receive(packet);
-		if (packet >> p2Position.x >> p2Position.y)
-			rect2.setPosition(p2Position);
-
-		Window.draw(rect1);
-		Window.draw(rect2);
-
-		Window.display();
-		Window.clear();
-
+		char answer = 'b';
+		do {
+			IpAddress rIP;
+			unsigned short port ;
+			if (socket.receive(packet, rIP, port) == Socket::Done) {
+				computerID[port] = rIP;
+			}
+			cout << "Enter to continue: (press a to continue)";
+			cin >> answer;
+		} while (answer != 'a');
 	}
 
+	else if (connectionType == 'c') {
+		string sIp;
+		cout << "Enter server ip: ";
+		cin >> sIp;
+		//sIp = "139.179.210.187";
+		IpAddress sendIP(sIp);
+		if (socket.send(packet, sendIP, 2000) == Socket::Done)
+			cout << "Socket SENT!!" << endl;
+		cout << sIp << endl;
+	}
 
+	bool done = false;
+	while (!done) {
+		if (connectionType == 's') {
+			getline(cin, text);
+			Packet packet;
+			packet << text;
+			map<unsigned short, IpAddress> ::iterator tempIterator;
+			for (tempIterator = computerID.begin(); tempIterator != computerID.end(); tempIterator++)
+				if (socket.send(packet, tempIterator->second, tempIterator->first) == Socket::Done)  {} // the socket send or not 
+				
+		}
+		else if (connectionType == 'c') {
+			IpAddress tempId;
+			unsigned short tempPort;
+			if (socket.receive(packet, tempId, tempPort) == Socket::Done) {			// The socket received or not 
+				string receivedText;
+				packet >> receivedText;
+				cout << "Received: " << receivedText << endl;
+			}
 
+		}
+	}
+		
+	
+	
+	
 	/*GM->placeSoldier("ahmet", "aksaray", 1);
 	GM->placeSoldier("ahmet", "ankara", 2);
 	GM->placeSoldier("ahmet", "konya", 4);
