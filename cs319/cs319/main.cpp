f1db7f073c0f7a516c5b38f199b133d9f0231a57
rumeysaozaydin <44906186+rumeysaozaydin@users.noreply.github.com>
2019-05-17 14:19:24 +0300

    Update main.cpp

--- cs319/cs319/main.cpp
+++ cs319/cs319/main.cpp
@@ -1,1 +1,21 @@
 
+#include "Top.h"
+
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+
+int main() {
+	WindowManager* WM = new WindowManager();
+
+	WM->GM->loadProvinces();
+
+	WM->createWindow();
+
+
+	return 0;
+}
